f7fa4647df5411b2ac1c10f38c283e17a87d2f90
NevilKumar2002 <127580266+NevilKumar2002@users.noreply.github.com>
2024-07-09 16:25:54 +0530

    Update MapComponent.js

--- frontend/src/components/MapComponent.js
+++ frontend/src/components/MapComponent.js
@@ -45,7 +45,7 @@ const MapComponent = () => {
       console.log("UserID:", userId);
 
       const response = await axios.patch(
-        `https://geo-data-mern-iak2.vercel.app/users/saveGeoJSON/${userId}`,
+        `https://geo-data-mern.onrender.com/users/saveGeoJSON/${userId}`,
         { GeoJSONData: geoData },
         {
           headers: {
